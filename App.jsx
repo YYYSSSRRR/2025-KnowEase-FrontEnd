2e14d1cf71e2ec9f250999fa61cbda2320567419
YYYSSSRRR <2163368320@qq.com>
2025-01-13 20:54:45 +0800

    插入图片

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+
+export default function App() {
+
+  return (
+    <ImageBackground
+      source={require('./bg.png')}
+      style={{zIndex:'-1'}}
+    >
+      <ImageBackground
+        source={require('./12320.png')}
+        style={{position:'absolute',top:'30%',left:'15%'}}
+      > 
+      </ImageBackground>
+      <Text
+        style={{color:'white',position:'absolute',top:'50%',left:'20%',fontSize:'20'
+        }}
+      >小知，你的校园生活小助手</Text>
+    </ImageBackground>
+  );
+}
+
